e1bddec129030f58a7b688f6a610090fe8fe4d3c
melekabbassi <abbassimelek@gmail.com>
2022-09-23 14:04:39 +0100

    added servlet.jar and added testServlet.java

---
+++ src/servlets/testServlet.java
@@ -0,0 +1,15 @@
+package servlets;   
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+
+class testServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("Hello World!");
+    }
+}
